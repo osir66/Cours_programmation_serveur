c04bb174c1b4b056a6c323f3ad7b72b0d4b96e01
ClementGentel <nec@gentel.fr>
2026-02-27 10:21:45 +0100

    ajout des routes dans l'app

--- Back/api.py
+++ Back/api.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from Back.routes import profRoute
+from Back.routes import profRoute, coursRoute, salleRoute, promotionRoute
 
 app = FastAPI()
 
+# Enregistrer tous les routers disponibles pour qu'ils apparaissent dans la doc OpenAPI (/docs)
 app.include_router(profRoute.router)
+app.include_router(coursRoute.router)
+app.include_router(salleRoute.router)
+app.include_router(promotionRoute.router)
 
 @app.get("/")
 def read_root():
