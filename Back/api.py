f4d8c4466749fa1d17fefb73145e556bf065d210
ClementGentel <nec@gentel.fr>
2026-03-16 09:09:42 +0100

    Page prof test front

--- Back/api.py
+++ Back/api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
 from Back.routes import profRoute, coursRoute, salleRoute, promotionRoute
 
 app = FastAPI()
@@ -9,6 +11,11 @@ app.include_router(coursRoute.router)
 app.include_router(salleRoute.router)
 app.include_router(promotionRoute.router)
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+INDEX_FILE = BASE_DIR / "Front" / "create_prof.html"
+
 @app.get("/")
 def read_root():
+    if INDEX_FILE.exists():
+        return FileResponse(str(INDEX_FILE))
     return {"message": "Hello FastAPI"}
