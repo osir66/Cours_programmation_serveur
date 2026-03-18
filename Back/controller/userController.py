a5302c1a72b8129f67bdc4d757d2a74448fab1c6
enzofaure013-blip <enzofaure013@gmail.com>
2026-03-18 11:43:19 +0100

    flake8

--- Back/controller/userController.py
+++ Back/controller/userController.py
@@ -3,9 +3,13 @@ from pathlib import Path
 
 DB_PATH = Path(__file__).resolve().parents[1] / "Base" / "database.db"
 
+
+
 def get_conn():
     conn = sqlite3.connect(str(DB_PATH), check_same_thread=False)
     conn.row_factory = sqlite3.Row
+
+
     return conn
 
 def getListUser():
@@ -14,6 +18,8 @@ def getListUser():
     cur.execute('''SELECT * FROM USER''')
     rows = [dict(row) for row in cur.fetchall()]
     cur.close()
+
+
     conn.close()
     return rows
 
@@ -21,6 +27,8 @@ def CreateUser(id_user,email,password,admin):
     conn = get_conn()
     cur = conn.cursor()
     cur.execute('''INSERT INTO USER (id_user,email,password,admin) VALUES(?,?,?,?)''',
+
+
                 (id_user,email,password,admin))
     rows = [dict(row) for row in cur.fetchall()]
     cur.close()
@@ -28,6 +36,8 @@ def CreateUser(id_user,email,password,admin):
     return rows
 
 def DeleteUser(id_user):
+
+
     conn = get_conn()
     cur = conn.cursor()
     cur.execute('''DELETE FROM prof WHERE id_user = ?''', (id_user,))
@@ -36,6 +46,8 @@ def DeleteUser(id_user):
     conn.close()
 
 def UpdateUser(id_user,email,password,admin):
+
+
     conn = get_conn()
     cur = conn.cursor()
     cur.execute('''UPDATE user SET email = ?, password = ?, admin = ? WHERE id_user = ?''',id_user,email,password,admin)
