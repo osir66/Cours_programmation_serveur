abf03e65bbfdab1ae57fa3121f102597d57e332c
enzofaure013-blip <enzofaure013@gmail.com>
2026-03-18 16:01:54 +0100

    commentaire

--- Back/controller/salleController.py
+++ Back/controller/salleController.py
@@ -1,22 +1,25 @@
 import sqlite3
 from pathlib import Path
 
+# Pointe vers Back/Base/database.db
 DB_PATH = Path(__file__).resolve().parents[1] / "Base" / "database.db"
 
+#fonction pour la connexion à la base de données
 def get_conn():
     conn = sqlite3.connect(str(DB_PATH), check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
+#fonction pour récupérer la liste des salles
 def getListSalles():
     conn = get_conn()
     cur = conn.cursor()
-    # On utilise bien id_salle, nom_salle et capacite
     cur.execute("SELECT id_salle AS id, nom_salle, capacite FROM salle")
     rows = [dict(row) for row in cur.fetchall()]
     conn.close()
     return rows
 
+#fonction pour créer les salles 
 def CreateSalle(nom_salle, capacite):
     conn = get_conn()
     cur = conn.cursor()
