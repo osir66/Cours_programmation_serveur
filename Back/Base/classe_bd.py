9a910509c8a3201ab37e8783cd1a0a785bcc4c35
enzofaure013-blip <enzofaure013@gmail.com>
2026-03-18 11:59:17 +0100

    bd Flake8

--- Back/Base/classe_bd.py
+++ Back/Base/classe_bd.py
@@ -1,57 +1,63 @@
-from pydantic import BaseModel
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, SQLModel
 from datetime import datetime
 
 
 #   -----------------------------------
-#   TABLE PROF 
+#   TABLE PROF
 #   -----------------------------------
 
-class Prof(SQLModel,table=True):
-    id_prof: int |None = Field(default=None,primary_key=True)
-    nom: str 
-    prenom: str 
+class Prof(SQLModel, table=True):
+    id_prof: int | None = Field(default=None, primary_key=True)
+    nom: str
+    prenom: str
 
 #   -----------------------------------
-#   TABLE PROMOTION  
+#   TABLE PROMOTION
 #   -----------------------------------
 
-class Promotion(SQLModel,table=True):
-    id_promo: int |None = Field(default=None,primary_key=True)
-    nom_promotion: str 
-    annee: int 
+class Promotion(SQLModel, table=True):
+    id_promo: int | None = Field(default=None, primary_key=True)
+    nom_promotion: str
+    annee: int
 
 #   -----------------------------------
-#   TABLE USER 
+#   TABLE USER
 #   -----------------------------------
 
-class User(SQLModel,table=True):
-    id_user: int |None = Field(default=None,primary_key=True)
-    email: str 
-    password: str 
-    admin: bool 
+class User(SQLModel, table=True):
+    id_user: int | None = Field(default=None, primary_key=True)
+    email: str
+    password: str
+    admin: bool
 
 #   -----------------------------------
-#   TABLE SALLE  
-#   -----------------------------------    
+#   TABLE SALLE
+#   -----------------------------------
 
-class Salle(SQLModel,table=True):
-    id_salle: int | None = Field(default=None,primary_key=True)
-    nom_salle: str 
-    capacite: int 
+class Salle(SQLModel, table=True):
+    id_salle: int | None = Field(default=None, primary_key=True)
+    nom_salle: str
+    capacite: int
 
 #   -----------------------------------
-#   TABLE COURS   
-#   -----------------------------------  
+#   TABLE COURS
+#   -----------------------------------
 
-class Cours(SQLModel,table=True):
-    id_cours: int | None = Field(default=None,primary_key=True)
-    matiere: str 
-    date_debut: datetime 
+class Cours(SQLModel, table=True):
+    id_cours: int | None = Field(default=None, primary_key=True)
+    matiere: str
+    date_debut: datetime
     date_fin: datetime
-    duree_total: datetime 
-    id_promo: int | None = Field(default=None,foreign_key="promotion.id_promo")
-    id_salle: int | None = Field(default=None,foreign_key="salle.id_salle")
-    id_prof: int | None = Field(default=None,foreign_key="prof.id_prof")
+    duree_total: datetime
+    id_promo: int | None = Field(
+        default=None, foreign_key="promotion.id_promo"
+    )
+    id_salle: int | None = Field(
+        default=None, foreign_key="salle.id_salle"
+    )
+    id_prof: int | None = Field(
+        default=None, foreign_key="prof.id_prof"
+    )
+
     
     
