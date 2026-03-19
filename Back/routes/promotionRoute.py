a6575d3cb8de1e409618185bf3761921a9e5a886
enzofaure013-blip <enzofaure013@gmail.com>
2026-03-19 16:27:57 +0100

    modif docs

--- Back/routes/promotionRoute.py
+++ Back/routes/promotionRoute.py
@@ -12,25 +12,25 @@ class PromotionCreate(BaseModel):
     annee: int
 
 # Route pour récupérer la liste de toutes les promotions
-@router.get("/getListPromotions", summary="Récupérer la liste de toutes les promotions")
+@router.get("/getListPromotions", summary="Récupérer la liste de toutes les promotions (back-end)")
 def get_list_promotions():
     return promotionController.getListPromotions()
 
 # Route pour ajouter une promotion
-@router.post("/createPromotion", summary="Ajouter une promotion")
+@router.post("/createPromotion", summary="Ajouter une promotion (back-end)")
 def add_promotion(promo: PromotionCreate):
     return promotionController.CreatePromotion(
         promo.nom_promotion, promo.annee
     )
 
 # Route pour modifier une promotion
-@router.put("/updatePromotion/{id_promo}", summary="Modifier une promotion")
+@router.put("/updatePromotion/{id_promo}", summary="Modifier une promotion (back-end)")
 def update_promotion(id_promo: int, promo: PromotionCreate):
     return promotionController.updatePromotion(
         id_promo, promo.nom_promotion, promo.annee
     )
 
 # Route pour supprimer une promotion
-@router.delete("/deletePromotion/{id_promo}", summary="Supprimer une promotion")
+@router.delete("/deletePromotion/{id_promo}", summary="Supprimer une promotion (back-end)")
 def delete_promotion(id_promo: int):
     return promotionController.deletePromotion(id_promo)    
